fb7b9750360bf242b467ad5cbc4fd25f3736bbc5
itsgale <mcetolentino0911@gmail.com>
2024-09-20 20:56:09 +0800

    Prelim Activities

---
+++ src/tolentinoprelim/Reciept.java
@@ -0,0 +1,36 @@
+
+package tolentinoprelim;
+
+import java.util.Scanner;
+
+
+
+public class Reciept {
+    public void getreceipt(){
+        
+        Scanner input = new Scanner(System.in);
+        String fname,pname;
+        
+        int quan, price, cash, totald,change;
+        
+        System.out.println("Customer name:");
+        fname = input.nextLine();
+        
+        System.out.println("Product Name:");
+        pname = input.nextLine();
+        
+        System.out.println("Quantity:");
+        quan = input.nextInt();
+        
+        System.out.println("Price:");
+        price = input.nextInt();
+        
+        System.out.println("Cash:");
+        cash = input.nextInt();
+        Reciepts rc = new Reciepts();
+        rc.processReceipt(fname, pname, quan, price, cash);
+        rc.viewReceipt();
+        
+    }
+    
+}
